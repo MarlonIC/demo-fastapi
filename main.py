2edb1455c724faec4c42293b9fa87dd4fc5a9ce7
MarlonIC <marlonricslim@gmail.com>
2022-11-08 01:58:53 -0500

    feat: add Model in api of method put

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from typing import Union
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
@@ -16,4 +23,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {
         "item_id": item_id,
         "q": q
+    }
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {
+        "item_name": item.name,
+        "item_price": item.price,
+        "item_id": item_id
     }
