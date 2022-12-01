2249f48f4d3c94bec32b729b6ccf6dccce8e3ffa
MarlonIC <marlonricslim@gmail.com>
2022-12-01 02:10:11 -0500

    doc: test api with docs in openapi

--- controllers/main.py
+++ controllers/main.py
@@ -504,3 +504,103 @@ async def create_multiple_images(images: List[Image]):
 @app.post("/index-weights/")
 async def create_index_weights(weights: Dict[int, float]):
     return weights
+
+
+class Item9(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2
+            }
+        }
+
+
+@app.put("/items30/{item_id}")
+async def update_item_30(item_id: int, item: Item9):
+    results = {
+        "item_id": item_id,
+        "item": item
+    }
+    return results
+
+
+class Item10(BaseModel):
+    name: str = Field(example="Foo")
+    description: Union[str, None] = Field(default=None, example="A very nice Item")
+    price: float = Field(example=35.4)
+    tax: Union[float, None] = Field(default=None, example=3.2)
+
+
+@app.put("/items31/{item_id}")
+async def update_item(item_id: int, item: Item10):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+class Item11(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@app.put("/items32/{item_id}")
+async def update_item(
+    item_id: int,
+    item: Item11 = Body(
+        example={
+            "name": "Foo",
+            "description": "A very nice Item",
+            "price": 35.4,
+            "tax": 3.2,
+        },
+    ),
+):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.put("/items33/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Item11 = Body(
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item 11",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
+        },
+    ),
+):
+    results = {"item_id": item_id, "item": item}
+    return results
