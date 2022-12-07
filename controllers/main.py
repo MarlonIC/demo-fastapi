a8bd0c21140c1cb2964d35a47014180400188e71
MarlonIC <marlonricslim@gmail.com>
2022-12-07 02:51:52 -0500

    feat: fastapi extra models

--- controllers/main.py
+++ controllers/main.py
@@ -754,3 +754,122 @@ async def read_item_name(item_id: str):
 @app.get("/items44/{item_id}/public", response_model=Item14, response_model_exclude={"tax"})
 async def read_item_public_data(item_id: str):
     return items2[item_id]
+
+
+# Extra Models
+class UserIn2(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class UserOut2(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn2):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user2/", response_model=UserOut2)
+async def create_user(user_in: UserIn2):
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+
+class UserIn3(UserBase):
+    password: str
+
+
+class UserOut3(UserBase):
+    pass
+
+
+class UserInDB2(UserBase):
+    hashed_password: str
+
+
+def fake_save_user2(user_in: UserIn3):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user3/", response_model=UserOut3)
+async def create_user(user_in: UserIn3):
+    user_saved = fake_save_user2(user_in)
+    return user_saved
+
+
+class BaseItem(BaseModel):
+    description: str
+    type: str
+
+
+class CarItem(BaseItem):
+    type = "car"
+
+
+class PlaneItem(BaseItem):
+    type = "plane"
+    size: int
+
+
+items3 = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
+
+
+@app.get("/items45/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+    return items3[item_id]
+
+
+class Item15(BaseModel):
+    name: str
+    description: str
+
+
+items4 = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
+
+
+@app.get("/items46/", response_model=List[Item15])
+async def read_items():
+    return items4
+
+
+@app.get("/keyword-weights/", response_model=Dict[str, float])
+async def read_keyword_weights():
+    return {"foo": 2.3, "bar": 3.4}
