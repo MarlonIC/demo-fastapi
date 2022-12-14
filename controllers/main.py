99cdcc2ee1feac0d2f1f83387470378249a0e5c8
MarlonIC <marlonricslim@gmail.com>
2022-12-14 02:21:56 -0500

    feat: fastapi body updates patch

--- controllers/main.py
+++ controllers/main.py
@@ -1089,3 +1089,41 @@ def update_item(id: str, item: Item17):
     json_compatible_item_data = jsonable_encoder(item)
     fake_db[id] = json_compatible_item_data
     print(fake_db)
+
+
+# Body Updates
+class Item18(BaseModel):
+    name: Union[str, None] = None
+    description: Union[str, None] = None
+    price: Union[float, None] = None
+    tax: float = 10.5
+    tags: List[str] = []
+
+
+items5 = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items61/{item_id}", response_model=Item18)
+async def read_item(item_id: str):
+    return items5[item_id]
+
+
+@app.put("/items62/{item_id}", response_model=Item18)
+async def update_item(item_id: str, item: Item18):
+    update_item_encoded = jsonable_encoder(item)
+    items5[item_id] = update_item_encoded
+    return update_item_encoded
+
+
+@app.patch("/items63/{item_id}", response_model=Item18)
+async def update_item(item_id: str, item: Item18):
+    stored_item_data = items5[item_id]
+    stored_item_model = Item18(**stored_item_data)
+    update_data = item.dict(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items5[item_id] = jsonable_encoder(updated_item)
+    return updated_item
