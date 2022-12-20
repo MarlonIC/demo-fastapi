5287b13f324662d2976c296e9836a521b5d794c7
MarlonIC <marlonricslim@gmail.com>
2022-12-20 02:08:51 -0500

    feat: fastapi classes as dependencies

--- controllers/main2.py
+++ controllers/main2.py
@@ -18,3 +18,38 @@ async def read_items(commons: dict = Depends(common_parameters)):
 @app.get("/users/")
 async def read_users(commons: dict = Depends(common_parameters)):
     return commons
+
+
+# Classes as Dependencies
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
+
+class CommonQueryParams:
+    def __init__(self, q: Union[str, None] = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items1/")
+async def read_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
+
+
+@app.get("/items2/")
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
