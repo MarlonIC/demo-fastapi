1961c201078207ed14aed3a925d56d9530c957a0
MarlonIC <marlonricslim@gmail.com>
2022-12-28 02:36:52 -0500

    feat: fastapi global dependencies

--- controllers/main2.py
+++ controllers/main2.py
@@ -89,4 +89,3 @@ async def verify_key(x_key: str = Header()):
 @app.get("/items4/", dependencies=[Depends(verify_token), Depends(verify_key)])
 async def read_items():
     return [{"item": "Foo"}, {"item": "Bar"}]
-
