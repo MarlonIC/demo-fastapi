475db1f0fbb15f8c7dcc07631a26c4a19ac5207b
MarlonIC <marlonricslim@gmail.com>
2022-12-28 02:23:00 -0500

    feat: fast api sub-dependencies

--- controllers/main2.py
+++ controllers/main2.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Cookie
 
 app = FastAPI()
 
@@ -53,3 +53,22 @@ async def read_items(commons: CommonQueryParams = Depends()):
     items = fake_items_db[commons.skip: commons.skip + commons.limit]
     response.update({"items": items})
     return response
+
+
+# Sub-dependencies
+def query_extractor(q: Union[str, None] = None):
+    return q
+
+
+def query_or_cookie_extractor(
+    q: str = Depends(query_extractor),
+    last_query: Union[str, None] = Cookie(default=None),
+):
+    if not q:
+        return last_query
+    return q
+
+
+@app.get("/items3/")
+async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {"q_or_cookie": query_or_default}
